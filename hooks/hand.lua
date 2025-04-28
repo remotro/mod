eb409ec776181ae2a3e640fb8874dc5f525cc99d
Alexander Capehart <alex@oxycblt.org>
2025-04-28 11:15:42 -0600

    mod/hooks: more card info

--- hooks/hand.lua
+++ hooks/hand.lua
@@ -5,10 +5,29 @@ function RE.Hands.get()
     local hand = G.hand;
     local json_hand = {};
     for i, v in ipairs(hand) do
+        local edition = "";
+        if card.edition.foil then
+            edition = "e_foil"
+        elseif card.edition.holo then
+            edition = "e_holo"
+        elseif card.edition.polychrome then
+            edition = "e_polychrome"
+        elseif card.edition.negative then
+            edition = "e_negative"
+        else
+            edition = "e_base"
+        end
+        local enhancement = card.ability.name;
         local card_info = v.base;
+        local rank = card_info.value;
+        local suit = card_info.suit;
+        local seal = card.seal;
         local json = {
-            rank = card_info.value,
-            suit = card_info.suit,
+            edition = edition,
+            enhancement = enhancement,
+            value = rank,
+            suit = suit,
+            seal = seal
         }
         table.insert(json_hand, json);
     end

86472cab28a55317f3ea6436a7eb431dbb2cf572
Isaac Hesslegrave <isaac.hesslegrave@gmail.com>
2025-05-11 17:10:26 +0100

    Add card data to buying functions

--- hooks/shop.lua
+++ hooks/shop.lua
@@ -10,10 +10,27 @@ function RE.Shop.Protocol.buy_main(request, ok, err)
 		err("Cannot do this action, must be in shop but in " .. G.STATE)
 		return
 	end
-	if not G.FUNCS.check_for_buy_space(c) then
+	local card = G.shop_jokers.cards[request.index]
+	if not G.FUNCS.check_for_buy_space(card.config.ref_table) then
 		err("Cannot do this action, No space")
 	end
-	if (e.config.ref_table.cost > G.GAME.dollars - G.GAME.bankrupt_at) and (e.config.ref_table.cost > 0) then
+	if (card.config.ref_table.cost > G.GAME.dollars - G.GAME.bankrupt_at) and (card.config.ref_table.cost > 0) then
+		Err("Not enough money")
+		return
+	end
+end
+
+function RE.Shop.Protocol.buy_and_use(request, ok, err)
+	if G.STATE ~= G.STATES.SHOP then
+		err("Cannot do this action, must be in shop but in " ..G.STATE)
+		return
+	end
+	local card = G.shop_jokers.cards[request.index]
+	if card.config.id ~= 'buy_and_use' then
+		err("Cannot do this action, card cannot be used right now")
+		return
+	end
+	if (card.config.ref_table.cost > G.GAME.dollars - G.GAME.bankrupt_at) and (card.config.ref_table.cost > 0) then
 		Err("Not enough money")
 		return
 	end
@@ -24,6 +41,11 @@ function RE.Shop.Protocol.buy_voucher(request, ok, err)
 		err("Cannot do this action, must be in shop but in " .. G.STATE)
 		return
 	end
+	local card = G.shop_jokers.cards[request.index]
+	if (card.config.ref_table.cost > G.GAME.dollars - G.GAME.bankrupt_at) and (card.config.ref_table.cost > 0) then
+		Err("Not enough money")
+		return
+	end
 end
 
 function RE.Shop.Protocol.buy_booster(request, ok, err)
@@ -31,19 +53,12 @@ function RE.Shop.Protocol.buy_booster(request, ok, err)
 		err("Cannot do this action, must be in shop but in " .. G.STATE)
 		return
 	end
-end
-
-function RE.Shop.Protocol.buy_and_use(request, ok, err)
-	if G.STATE ~= G.STATES.SHOP then
-		err("Cannot do this action, must be in shop but in " ..G.STATE)
-		return
-	end
-	if e.config.id ~= 'buy_and_use' then
-		err("Cannot do this action, card cannot be used right now")
+	local card = G.shop_jokers.cards[request.index]
+	if (card.config.ref_table.cost > G.GAME.dollars - G.GAME.bankrupt_at) and (card.config.ref_table.cost > 0) then
+		Err("Not enough money")
 		return
 	end
 end
-
 
 function RE.Shop.Protocol.reroll(request, ok, err)
 	if G.STATE ~= G.STATES.SHOP then
@@ -51,7 +66,7 @@ function RE.Shop.Protocol.reroll(request, ok, err)
 		return
 	end
 	if ((G.GAME.dollars-G.GAME.bankrupt_at) - G.GAME.current_round.reroll_cost < 0) and G.GAME.current_round.reroll_cost ~= 0 then
-		err("Cannot afford to reroll shop")
+		err("Not enough money")
 		return
 	end
 	G.FUNCS.reroll_shop()
@@ -62,5 +77,5 @@ function RE.Shop.Protocol.continue(request, ok, err)
 		err("Cannot do this action, must be in shop but in " .. G.STATE)
 		return
 	end
-
-
+	G.FUNCS.toggle_shop()
+end

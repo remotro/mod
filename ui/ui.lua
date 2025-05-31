28be10d352474af3740cfec2fc3e91ed7f7f63f8
Isaac Hesslegrave <isaac.hesslegrave@gmail.com>
2025-06-01 00:38:19 +0100

    Combine UI files into one file

--- ui/ui.lua
+++ ui/ui.lua
@@ -84,3 +84,13 @@ SMODS.Mods.Remotro.config_tab = function()
 		}
 	}
 end
+function G.FUNCS.remotro_reconnect()
+	if RE.Client.connected() then
+		love.thread.getChannel("uiToNetwork"):push("disconnect!")
+	else
+		if not RE.NETWORKING_THREAD then
+			RE.net_thread_start()
+		end
+		RE.Client.connect()
+	end
+end

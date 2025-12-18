f4099383012651c5923eb0a8337e69739e3cd2d0
Isaac Hesslegrave <isaac.hesslegrave@gmail.com>
2025-12-18 22:36:33 +0000

    Fix function call and decrease initial connection delay

--- vendor/socket/socket.lua
+++ vendor/socket/socket.lua
@@ -45,7 +45,7 @@ function Networking.connect()
 
 	Networking.Client = socket.tcp()
 	-- Allow for 10 seconds to reconnect
-	Networking.Client:settimeout(10)
+	Networking.Client:settimeout(5)
 
 	Networking.Client:setoption("tcp-nodelay", true)
 	local connectionResult, errorMessage = Networking.Client:connect(CONFIG_URL, CONFIG_PORT) -- Not sure if I want to make these values public yet
